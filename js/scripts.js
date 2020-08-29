e810b6800473cffde08d4f34952b2bce639e89e5
Adrian Braga <adrian.braga.ro@gmail.com>
2020-08-29 19:41:53 -0400

    controle de maximizar e desmaximizar com js puro aplicado

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 const modalOverlay = document.querySelector('.modal-overlay');
+const modalMax = document.querySelector('.modal');
 const cardsBootcamp = document.querySelectorAll('.card-bootcamp');
 
 // seleção do card para adicionar a classe active e abrir o modal junto com seus respectivos IDs.
@@ -13,8 +14,18 @@ for (let cardBootcamp of cardsBootcamp) {
     })
 }
 
+// Controle de maximizar e desmaximizar tamanho do iframe.
+document.querySelector('.max-modal').addEventListener("click", () => {
+    if (modalMax.classList.contains('maximize')) {
+        modalMax.classList.remove('maximize')
+    } else {
+        modalMax.classList.add('maximize')
+    }
+});
+
 // removendo a classe active e limpando o "src"
 document.querySelector('.close-modal').addEventListener("click", () => {
     modalOverlay.classList.remove('active')
+    modalMax.classList.remove('maximize')
     modalOverlay.querySelector( 'iframe' ).src = "" ;
 });
